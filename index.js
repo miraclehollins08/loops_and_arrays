1ed3df0756e2bed3bc54a5e2de58fd073660970b
Miracle <54377927+miraclehollins08@users.noreply.github.com>
2020-01-10 01:56:33 -0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+var officeItems = ["stapler", "monitor", "computer", "desk", "lamp", "computer", "lamp", "stapler", "computer", "computer"]
+
+var count = 0
+
+while(count < 4){
+    console.log('computer')
+    count++
+}
+
+
+var pepoleWhoWantToSeeMadMaxFuuryRoad = [
+    {
+        name: "Mike",
+        age: 12,
+        gender: "male"  
+    },{
+        name: "Madeline",
+        age: 80,
+        gender: "female"
+    },{
+        name: "Cheryl",
+        age: 22,
+        gender: "female"
+    },{
+        name: "Sam",
+        age: 30,
+        gender: "male"
+    },{
+        name: "Suzy",
+        age: 4,
+        gender: "female"
+    }
+]
+
+var age = [18];
+if(age >= 80,22,30){
+    console.log("old enough");
+    console.log("Madeline is old enough to see Mad Max");
+    console.log("Madeline is old enough. She's good to see Mad Max Fury Raod")
+}
+
+var age = [18];
+if(age < 12,4){
+    console.log("not old enough");
+    console.log("Mike is not old enough to see Mad Max");
+    console.log("Mike is not old enough to see Mad Max Fury Road, don't let Him in")
+}
+
+for(var a = 1; a <= 101; a++){
+    if(a % 2 != 0){
+        console.log (a + "Odd");
+    }
+}
+
+for(var a = 1; a <= 101; a++){
+    if(a % 2 == 0){
+        console.log (a + "Even");
+    }
+}
